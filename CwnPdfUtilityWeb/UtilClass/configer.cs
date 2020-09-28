774a11adf367d34e73888f0d77cbe12dfc72e7f5
PunjapholKengna@outlook.co.th <PunjapholKengna@outlook.co.th>
2020-09-28 08:11:12 +0700

    test

---
+++ CwnPdfUtilityWeb/UtilClass/configer.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configer.Customize
+{
+    enum DBTYPE
+    {
+        MSSQL, MSACCESS, MYSQL, ORACLE, EXCEL, CSV
+    }
+    struct connectDbType
+    {
+        public String ConnectionString { get; set; }
+        public DBTYPE CondbType { get; set; }
+    }
+    class configer
+    {
+
+        public connectDbType ConfigSvr87 { get; set; }
+     
+        public configer()
+        {
+            this.ConfigSvr87 = new connectDbType { ConnectionString = "Data Source = 192.168.1.87\\; Initial Catalog = Traceability-TEST; Persist Security Info = True; User ID = sa; Password = SysteM2551", CondbType = DBTYPE.MSSQL };
+        }
+
+    }
+}
